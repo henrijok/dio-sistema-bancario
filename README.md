f42f9205bc79bd73df912cd28978f8c24a06b297
Henrique Garcia <henriesp@gmail.com>
2025-03-04 17:19:51 -0300

    Primeiro commit

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# dio-sistema-bancario
-Criar um sistema bancário com as operações: sacar, depositar e visualizar extrato.
+# SISTEMA BANCÁRIO
+<div>
+Descrição: Criar um sistema bancário com as operações: sacar, depositar e visualizar extrato.
+</div>
+
+<div>
+Autor: Henrique Esposito Garcia
+</div>
+
+<div>
+Data: 03/2025
+</div>
+
+## Slide do Desafio:
+
+[![Slide](https://img.shields.io/badge/Slide-0077B5?style=for-the-badge&logo=Slide&logoColor=white)](https://academiapme-my.sharepoint.com/:p:/g/personal/kawan_dio_me/Ef-dMEJYq9BPotZQso7LUCwBJd7gDqCC2SYlUYx0ayrGNQ?e=G79e2L)
+
+## Habilidades Utilizadas:
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=white)
+![Git](https://img.shields.io/badge/GIT-E44C30?style=for-the-badge&logo=git&logoColor=white)
+![Vscode](https://img.shields.io/badge/Vscode-007ACC?style=for-the-badge&logo=visual-studio-code&logoColor=white)
+![Windows](https://img.shields.io/badge/Windows-000?style=for-the-badge&logo=windows&logoColor=2CA5E0)
+
+## Conecte-se comigo:
+[![LinkedIn](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/henrique-esposito-garcia-40031b2a4/)
+[![GitHub](https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white)](https://github.com/henrijok) 
+[![Gmail](https://img.shields.io/badge/Gmail-333333?style=for-the-badge&logo=gmail&logoColor=whit)](mailto:henriesp@gmail.com)
---
+++ sistema_bancario.py
@@ -0,0 +1,118 @@
+menu="""
+============= MENU =============
+
+1 - Depositar
+2 - Sacar
+3 - Visualizar Extrato
+0 - Sair
+
+================================
+"""
+
+saldo = 0
+limite = 500
+count_saque = 0
+
+saques = []
+depositos = []
+
+#---------------------------------------------------------------------------
+def main():
+#---------------------------------------------------------------------------
+    while True:
+        print(
+            """
+            ============= MENU =============
+
+            1 - Depositar
+            2 - Sacar
+            3 - Visualizar Extrato
+            0 - Sair
+
+            ================================
+        """
+        )
+
+        opcao = int(input("Digite o número do menu que deseja acessar: "))
+
+        if opcao == 1:
+            depositar()
+        elif opcao == 2:
+            sacar()
+        elif opcao == 3:
+            visualizar_extrato()
+        elif opcao == 0:
+            break
+        else:
+            print("Número inválido. Digite outro número para continuar...")
+
+#---------------------------------------------------------------------------
+def sacar():
+#---------------------------------------------------------------------------
+    global count_saque
+    global saldo
+    global limite
+    global saques
+
+    valor = float(input("Digite o valor que deseja sacar: "))
+
+    if count_saque >= 3:
+        print("Você já realizou 3 saques hoje. Tente novamente amanhã!")
+    elif (saldo <= 0) or (valor > saldo):
+        print(f"Seu saldo é de R$ {saldo:.2f}. Não é possivel realizar o saque!")
+    elif valor > limite:
+        print(f"Não foi possivel realizar a transação. Limite de saque R$ {limite:.2f}.")
+    else:
+        saldo -= valor
+        saques.append(f'- R$ {valor:.2f}')
+        count_saque += 1
+        print(f"Saque de R$ {valor:.2f} realizado com sucesso!")
+        print(f"Saldo Atual: {saldo:.2f}")
+
+#---------------------------------------------------------------------------
+def depositar():
+#---------------------------------------------------------------------------
+    global count_saque
+    global saldo
+    global depositos
+
+    valor = float(input("Digite o valor que deseja depositar: "))
+    
+    if valor <= 0:
+        print("Você digitou um valor inválido! Digite valores acima de R$ 0,00")
+    else:
+        saldo += valor
+        depositos.append(f'+ R$ {valor:.2f}')
+        print(f"Deposito de R$ {valor:.2f} realizado com sucesso!")
+        print(f"Saldo Atual: {saldo:.2f}")
+
+
+#---------------------------------------------------------------------------
+def visualizar_extrato():
+#---------------------------------------------------------------------------
+    global saldo
+    global saques
+    global depositos
+
+    if saques or depositos: 
+        print("=" * 40)
+        print("EXTRATO".center(40))
+        print("=" * 40)
+
+        for deposito in depositos:
+            print(f"DEPÓSITO: {deposito.rjust(30)}")  
+
+        for saque in saques:
+            print(f"SAQUE:    {saque.rjust(30)}")
+
+        print("=" * 40)
+        print(f"SALDO:    {f'R$ {saldo:.2f}'.rjust(30)}")
+        print("=" * 40)
+
+    else:
+        print("Não foram realizadas movimentações!")
+
+
+#---------------------------------INICIO--------------------------------
+if __name__ == "__main__":
+    main()
